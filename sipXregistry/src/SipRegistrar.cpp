mranga/sipxecs

commit 0ea4190f03afc79ce58f8df610ac00f0cf979b13

The registrar will now exit if the XmlRpc port is occupied.
git-svn-id: https://sipxecs.sipfoundry.org/rep/sipXecs/main@11719 ab1d8caa-1f67-47f1-9e81-24633a41865c

sipXregistry/src/SipRegistrar.cpp
@@ -155,25 +155,32 @@ SipRegistrar::SipRegistrar(OsConfigDb* configDb) :
 
 int SipRegistrar::run(void* pArg)
 {
-   startRpcServer();
-
-   /*
-    * If replication is configured,
-    *   the following blocks until the state of each peer is known
-    */
-   startupPhase(); 
- 
+   UtlBoolean bFatalError = false;
    int taskResult = 0;
    
-   if (!isShuttingDown())
+   startRpcServer(bFatalError);
+   if (!bFatalError)
    {
-      // Exit if the operational phase fails (e.g. SipUserAgent reports
-      // problems)
-      if (operationalPhase())
+      /*
+       * If replication is configured,
+       *   the following blocks until the state of each peer is known
+       */
+      startupPhase(); 
+       
+      if (!isShuttingDown())
       {
-         // from here on, everything happens in handleMessage
-         taskResult = OsServerTask::run(pArg);
+         // Exit if the operational phase fails (e.g. SipUserAgent reports
+         // problems)
+         if (operationalPhase())
+         {
+            // from here on, everything happens in handleMessage
+            taskResult = OsServerTask::run(pArg);
+         }
       }
+   }
+   else
+   {
+       OsSysLog::add(FAC_SIP, PRI_EMERG, "Unable to startup Rpc server (port in use?)\n");
    }
 
    return taskResult;
@@ -319,6 +326,12 @@ UtlBoolean SipRegistrar::operationalPhase()
    startRegistrarServer();
    startRedirectServer();
    startEventServer();
+
+   if (!mSipUserAgent->isOk())
+   {
+      OsSysLog::add(FAC_SIP, PRI_EMERG,
+            "SipUserAgent reported a problem while starting up (port in use?)");
+   }
 
    return mSipUserAgent->isOk() ;
 }
@@ -681,16 +694,22 @@ const UtlString& SipRegistrar::primaryName() const
 
 
 /// Server for XML-RPC requests
-void SipRegistrar::startRpcServer()
+void SipRegistrar::startRpcServer(UtlBoolean& bFatalError)
 {
+    bFatalError = false; // disable is not a fatal error
+   
    // Begins operation of the HTTP/RPC service
    // sets mHttpServer and mXmlRpcDispatcher
-
    if (mReplicationConfigured)
    {
       // Initialize mHttpServer and mXmlRpcDispatch
       mXmlRpcDispatch = new XmlRpcDispatch(mHttpPort, true /* use https */, mBindIp);
       mHttpServer = mXmlRpcDispatch->getHttpServer();
+      if (!mHttpServer->isSocketOk())
+      {
+         OsSysLog::add(FAC_SIP, PRI_ERR, "XmlRpc HttpServer failed to initialize listening socket");
+         bFatalError = true ;
+      }
    }
 }
 

sipXregistry/include/registry/SipRegistrar.h
@@ -80,7 +80,7 @@ public:
     /**< Messages are dispatched to either the SipRegistrarServer or SipRedirectServer thread */
 
     /// Server for XML-RPC requests
-    void startRpcServer();
+    void startRpcServer(UtlBoolean& bFatalError);
     /**<
      * Begins operation of the HTTP/RPC service
      * sets mHttpServer and mXmlRpcDispatch

sipXtackLib/include/net/HttpServer.h
@@ -139,6 +139,9 @@ public:
 /* ============================ ACCESSORS ================================= */
 
 /* ============================ INQUIRY =================================== */
+        
+    /// Is the server socket used by this HttpServer ok?
+    UtlBoolean isSocketOk() const ;    
 
 /* //////////////////////////// PROTECTED ///////////////////////////////// */
 protected:

sipXtackLib/src/net/HttpServer.cpp
@@ -215,6 +215,18 @@ OsStatus HttpServer::getStatus()
         return httpStatus;
 }
 
+UtlBoolean HttpServer::isSocketOk() const
+{
+    UtlBoolean bOk = FALSE;
+    
+    if (mpServerSocket)
+    {
+        bOk = mpServerSocket->isOk();
+    }
+        
+    return bOk;    
+}
+
 int HttpServer::run(void* runArg)
 {
     OsConnectionSocket* requestSocket = NULL;

commit 12dbd28591305c047d449632c6a7a856bdcb0d9c

Fix bug in registry replication introduced by r11719 the RPC path was not set correctly in the XmlRpcDispatcher.
git-svn-id: https://sipxecs.sipfoundry.org/rep/sipXecs/main@11749 ab1d8caa-1f67-47f1-9e81-24633a41865c

sipXregistry/src/SipRegistrar.cpp
@@ -709,7 +709,9 @@ void SipRegistrar::startRpcServer(UtlBoolean& bFatalError)
    if (mReplicationConfigured)
    {
       // Initialize mHttpServer and mXmlRpcDispatch
-      mXmlRpcDispatch = new XmlRpcDispatch(mHttpPort, true /* use https */, mBindIp);
+      mXmlRpcDispatch = new XmlRpcDispatch(mHttpPort, true /* use https */,
+                                           XmlRpcDispatch::DEFAULT_URL_PATH,
+                                           mBindIp);
       mHttpServer = mXmlRpcDispatch->getHttpServer();
       if (!mHttpServer->isSocketOk())
       {